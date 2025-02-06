e2cbc288081d785c3d33ecd0a6bab4eb1330e7df
Manabu-Aihara <manabu@yoboiryo.co.jp>
2025-02-06 09:24:22 +0900

    #250206: merge third

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,50 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { build, mergeConfig } from "vite";
+// @ts-check
+/**
+ * @type { import("@storybook/react/types").StorybookConfig}
+ */
+// module.exports = {
+//   stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
+//   addons: [
+//     '@storybook/addon-essentials',
+//     "storybook-addon-module-mock",
+//     '@storybook/addon-mdx-gfm',
+//     '@chromatic-com/storybook'
+//   ],
+//   core: {
+//     builder: {
+//       name: '@storybook/builder-vite',
+//     },
+//   },
+//   async viteFinal(config, { configType }) {
+//     const { mergeConfig } = await import('vite');
+//     config.plugins = [...config.plugins]
+//     if (process.env.NODE_ENV === 'production') {
+//       config.build.chunkSizeWarningLimit = 1200
+//     }
+//     return mergeConfig(config, {
+//       // Add dependencies to pre-optimization
+//       // optimizeDeps: {
+//       //   include: ['storybook-dark-mode'],
+//       // },
+//     });
+//   },
+
+//   framework: {
+//     name: '@storybook/react-vite',
+//     options: {}
+//   },
+
+//   docs: {},
+
+//   typescript: {
+//     reactDocgen: 'react-docgen-typescript'
+//   }
+// };
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -8,7 +53,21 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    // {
+    //   name: 'storybook-addon-vite-mock',
+    //   options: {
+    //     exclude: ({ id }) => id.includes(".stories."),
+    //     // debugPath: "tmp",
+    //   },
+    // }
   ],
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      build: {
+        chunkSizeWarningLimit: 100000000
+      },
+    })
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
